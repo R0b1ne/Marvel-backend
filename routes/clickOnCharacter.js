8ba36255ee84cb42a453ee2bf53db0dc5d98fa8d
Robine <Robine@macbook-robine-1.home>
2023-09-01 12:51:48 +0200

    comics description added in clickOnCharacter route

--- routes/clickOnCharacter.js
+++ routes/clickOnCharacter.js
@@ -7,11 +7,26 @@ router.get("/character/:characterId", async (req, res) => {
   try {
     const characterId = req.params.characterId;
     // console.log("Route Find a Character");
-    const response = await axios.get(
+    const responseCharacters = await axios.get(
       `https://lereacteur-marvel-api.herokuapp.com/character/${characterId}?apiKey=${process.env.API_KEY}`
     );
-    console.log(response.data);
-    res.json(response.data);
+
+    const comicsDetails = [];
+    for (let i = 0; i < responseCharacters.data.comics.length; i++) {
+      const comicId = responseCharacters.data.comics[i];
+      const responseComic = await axios.get(
+        `https://lereacteur-marvel-api.herokuapp.com/comic/${comicId}?apiKey=${process.env.API_KEY}`
+      );
+      comicsDetails.push(responseComic.data);
+    }
+
+    const characterData = {
+      character: responseCharacters.data,
+      comics: comicsDetails,
+    };
+
+    console.log(characterData);
+    res.json(characterData);
   } catch (error) {
     console.log(error.message);
   }
